ee535c6b716864f8b6faa1694a29336a93928e23
Faraz Fayyaz <113796246+farazfayyaz@users.noreply.github.com>
2023-05-21 23:31:39 -0400

    Update Buttons.jsx

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -11,7 +11,7 @@ export const Buttons = (props) => {
         <a className='Button' href={props.link}>
 
             <p className='Button-name'>{props.name}</p>
-            <img className='Button-icon' src={props.icon} />
+            <img className='Button-icon' src={props.icon} alt={props.name} />
 
         </a>
     </div>
